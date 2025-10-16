1e31a96254feae8c7451c40692317ad957055d9c
kanciltob412 <kanciltob@gmail.com>
2025-10-16 19:07:18 +0800

    Contact: use provided Google Maps embed (no external link)

--- resources/js/pages/Contact.tsx
+++ resources/js/pages/Contact.tsx
@@ -10,17 +10,14 @@ export default function Contact() {
             <div className="w-full">
                 <iframe
                     title="PORDAMSA - Location"
-                    src="https://maps.app.goo.gl/i2RRPCdiggcZmVxy7"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3943.9444146656656!2d115.176386!3d-8.6968289!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd246d9dcc47717%3A0xc4e5bc5834806352!2sJl.%20Sunset%20Road%20No.22%2C%20Legian%2C%20Kec.%20Kuta%2C%20Kabupaten%20Badung%2C%20Bali%2080361!5e0!3m2!1sen!2sid!4v1760612670287!5m2!1sen!2sid"
                     width="100%"
-                    height="360"
+                    height="450"
                     style={{ border: 0 }}
-                    allowFullScreen={false}
+                    allowFullScreen
                     loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
                 ></iframe>
-
-                <div className="text-center mt-4">
-                    <a href="https://maps.app.goo.gl/i2RRPCdiggcZmVxy7" target="_blank" rel="noopener noreferrer" className="text-sm text-gray-600 underline">View on Google Maps</a>
-                </div>
             </div>
 
             <main className="max-w-4xl mx-auto py-12 px-4 text-center">
