2d4e3542b56a4dd16790544eda64a91f8957d013
kanciltob412 <kanciltob@gmail.com>
2025-10-16 19:14:40 +0800

    Contact: use contact-lavanya.jpg and add spacing above map

--- resources/js/pages/Contact.tsx
+++ resources/js/pages/Contact.tsx
@@ -8,7 +8,7 @@ export default function Contact() {
 
             {/* Banner (matching About/Craftmanship) */}
             <div className="relative h-[400px] md:h-[420px] overflow-hidden">
-                <img src="/images/about/about-us-banner.jpg" alt="Contact banner" className="absolute w-full h-full object-cover" style={{ filter: 'brightness(0.6)' }} />
+                <img src="/contact-lavanya.jpg" alt="Contact banner" className="absolute w-full h-full object-cover" style={{ filter: 'brightness(0.6)' }} />
                 <div className="absolute inset-0 flex items-center">
                     <div className="max-w-6xl w-full mx-auto px-4 transform translate-y-12 md:translate-y-16 text-white">
                         <h1 className="text-4xl md:text-5xl font-semibold uppercase tracking-wide">CONTACT US</h1>
@@ -17,7 +17,7 @@ export default function Contact() {
             </div>
 
             {/* Full-width map */}
-            <div className="w-full">
+            <div className="w-full mt-8 md:mt-12">
                 <iframe
                     title="PORDAMSA - Location"
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3943.9444146656656!2d115.176386!3d-8.6968289!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd246d9dcc47717%3A0xc4e5bc5834806352!2sJl.%20Sunset%20Road%20No.22%2C%20Legian%2C%20Kec.%20Kuta%2C%20Kabupaten%20Badung%2C%20Bali%2080361!5e0!3m2!1sen!2sid!4v1760612670287!5m2!1sen!2sid"
