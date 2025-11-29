1ad56aaefd743ca35f6cf2ddc904084802cb3786
kanciltob412 <kanciltob@gmail.com>
2025-11-29 14:00:26 +0800

    Add PHPDoc annotations to TestCase for Laravel test helper methods

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,18 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Testing\TestResponse;
 
+/**
+ * @method TestResponse get(string $uri, array $headers = [])
+ * @method TestResponse post(string $uri, array $data = [], array $headers = [])
+ * @method TestResponse put(string $uri, array $data = [], array $headers = [])
+ * @method TestResponse patch(string $uri, array $data = [], array $headers = [])
+ * @method TestResponse delete(string $uri, array $headers = [])
+ * @method TestResponse actingAs(\Illuminate\Contracts\Auth\Authenticatable $user, $guard = null)
+ * @method void assertAuthenticated($guard = null)
+ * @method void assertGuest($guard = null)
+ */
 abstract class TestCase extends BaseTestCase
 {
     //
